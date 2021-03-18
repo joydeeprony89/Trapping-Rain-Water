028df6de75ddd3ed5ef4fafb4d2dfce57558d1d3
Jaydeep Shil <ED8A@eurofins.com>
2021-03-18 23:01:14 +0530

    Add project files.

---
+++ Trapping Rain Water/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Trapping_Rain_Water
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = new int[] { 0,1,0,2,1,0,1,3,2,1,2,1};
+            Console.WriteLine(Trap(result));
+        }
+
+        static int Trap(int[] height)
+        {
+            if (height == null || height.Length == 0) return 0;
+            int l = 0, r = height.Length - 1, level = 0, res = 0;
+            while (l < r)
+            {
+                int lower = height[height[l] < height[r] ? l++ : r--];
+                level = Math.Max(lower, level);
+                res += level - lower;
+            }
+            return res;
+        }
+    }
+}
